fed99d1efa714f667defb66b4624d28668e67fac
jbeckenbach <jbeckenbach@172.27.20.72>
2018-03-26 10:14:32 -0600

    Save

---
+++ JavaApplication1/src/Pinball/Pinballrunner.java
@@ -0,0 +1,29 @@
+package Pinball;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+public class Pinballrunner
+{
+public static void main(String[] args)
+{
+
+JFrame frame = new JFrame();
+
+
+frame.setSize(1920,1080);
+
+
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+Panel00 panel = new Panel00();
+   
+
+frame.add(panel);
+
+frame.setVisible(true);
+
+
+}
+}
+
